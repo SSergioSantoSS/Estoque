c38890f0b649a6049ea2a983211bce5109806051
Sergio Santos <sergiomi87st@gmail.com>
2021-02-17 19:34:01 -0300

    termino do projeto

--- ControleEstoque/Produto.cs
+++ ControleEstoque/Produto.cs
@@ -14,6 +14,17 @@ namespace ControleEstoque
         public double ValorTotalEmEstoque()
         {
             return Preco * Quantidade;
+
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos ( int quantidade)
+        {
+            Quantidade -= quantidade;
         }
 
             public override string ToString()
