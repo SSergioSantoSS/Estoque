b4425c2dfc3a5bdd3df8a874a17110bdea878151
Sergio Santos <sergiomi87st@gmail.com>
2021-02-17 15:47:54 -0300

    projeto iniciado com criação de classe Produto

---
+++ ControleEstoque/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ControleEstoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
